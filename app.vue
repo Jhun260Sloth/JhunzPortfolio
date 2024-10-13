bfeffd323e7b152fbd821654fd1616927a2cd3ec
Jhunnel <99332717+Jhun260Sloth@users.noreply.github.com>
2024-10-13 14:18:15 +0800

    com1

---
+++ app.vue
@@ -0,0 +1,15 @@
+<template>
+  <div>
+
+    <NuxtLayout>
+      <NuxtPage />
+    </NuxtLayout>
+
+    <!-- <AppAlert>
+      This is an auto-imported component.
+    </AppAlert> -->
+
+
+
+  </div>
+</template>
